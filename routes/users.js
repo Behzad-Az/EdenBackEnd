15984caef729e60f837293d337da87b754098378
Behzad-Az <behy1368@gmail.com>
2022-05-04 18:28:29 -0700

    created get :id and delete :id routes

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,34 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-const users = [
-  {
-    firstName: "John",
-    lastName: "Doe",
-    age: 25
-  },
-  {
-    firstName: "Jane",
-    lastName: "Doe",
-    age: 24
-  }
-];
+const users = [];
 
 router.get('/', (req, res) => {
   res.send(users);
 });
 
 router.post('/', (req, res) => {
-  const user = req.body;
+  const user = {
+    id: uuidv4(),
+    ...req.body
+  };
   users.push(user);
-  res.send(`User with first name ${user.firstName} added to database`);
+  res.send(`Inserted user with ID ${user.id}`);
+});
+
+router.get('/:id', (req, res) => {
+  const foundUser = users.find(user => user.id === req.params.id);
+  res.send(foundUser);
+});
+
+router.delete('/:id', (req, res) => {
+  const userIndex = users.findIndex(user => user.id === req.params.id);
+  if (userIndex > -1) {
+    users.splice(userIndex, 1);
+  }
+  res.send(`User with the id ${req.params.id} removed from database`);
 });
 
 export default router;
